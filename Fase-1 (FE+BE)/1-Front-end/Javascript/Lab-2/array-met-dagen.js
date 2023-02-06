164f7a26ae155ab33319dd898d3be59b9d48f500
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-02-06 12:57:00 +0100

    Array-met-degen

--- Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-2/array-met-dagen.js
+++ Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-2/array-met-dagen.js
@@ -1,1 +1,9 @@
-function a
+var daysOfWeek = ["Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag"];
+
+document.write("<h2>Alle dagen van de week zijn:</h2>");
+document.write(daysOfWeek.join(", "));
+
+document.write("<h2>De werkdagen zijn:</h2>");
+document.write(daysOfWeek.slice(0, 5).join(", "));
+
+console.log(daysOfWeek.slice(0, 5));
