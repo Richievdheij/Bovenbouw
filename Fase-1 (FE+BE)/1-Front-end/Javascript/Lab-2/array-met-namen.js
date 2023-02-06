397f3a4c994392ce18195c80491af8fab335b4bd
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-02-06 13:26:24 +0100

    array-met-namen
    Via een prompt stel je aan de gebruiker de volgende vraag: 'Hoeveel namen wilt u in de array stoppen? (minimaal 3)'. Nadat je de namen hebt ingevoerd is de uitvoer naar het scherm als volgt:
    
    Kopje h2: 'De ingevoerde namen in de array zijn:' met daaronder alle ingevoerde namen.
    
    Kopje h2: 'De ingevoerde namen in de array in omgekeerde volgorde zijn:' met daaronder alle ingevoerde namen in omgekeerde volgorde.

--- Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-2/array-met-namen.js
+++ Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-2/array-met-namen.js
@@ -1,17 +1,17 @@
 const hoeveelNamen = parseInt(prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)"));
 
 if (hoeveelNamen >= 3) {
-  let names = [];
+  let naam = [];
 
   for (let i = 0; i < hoeveelNamen; i++) {
-    names[i] = prompt(`Voer naam ${i + 1} in:`);
+    naam[i] = prompt(`Voer naam ${i + 1} in:`);
   }
 
   document.write("<h2>De ingevoerde namen in de array zijn:</h2>");
-  document.write(names.join(", "));
+  document.write(naam.join(", "));
 
   document.write("<h2>De ingevoerde namen in de array in omgekeerde volgorde zijn:</h2>");
-  document.write(names.reverse().join(", "));
-} else {
+  document.write(naam.reverse().join(", ")); }
+    else {
   alert("U moet minimaal 3 namen invoeren!");
 }
