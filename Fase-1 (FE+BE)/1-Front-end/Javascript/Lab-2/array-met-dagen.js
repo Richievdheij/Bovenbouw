cde5192b47101b49b35676da192d01a8e4f65686
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-02-06 13:08:30 +0100

    array-met-degen
    Maak een array aan waarin de dagen van de week zitten: maandag t/m zondag. De volgende uitvoer laat je op je scherm zien waarbij je gebruik moet maken van het 'aanspreken' van de array met de dagen die je hebt aangemaakt.

--- Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-2/array-met-dagen.js
+++ Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-2/array-met-dagen.js
@@ -6,4 +6,14 @@ document.write(daysOfWeek.join(", "));
 document.write("<h2>De werkdagen zijn:</h2>");
 document.write(daysOfWeek.slice(0, 5).join(", "));
 
-console.log(daysOfWeek.slice(0, 5));
+document.write("<h2>De weekenddagen zijn:</h2>");
+document.write(daysOfWeek.slice(5, 7).join(", "));
+
+document.write("<h2>Alle dagen van de week in omgekeerde volgorde zijn:</h2>");
+document.write(daysOfWeek.reverse().join(", "));
+
+document.write("<h2>De werkdagen in omgekeerde volgorde zijn:</h2>");
+document.write(daysOfWeek.slice(2, 7).reverse().join(", "));
+
+document.write("<h2>De weekenddagen in omgekeerde volgorde zijn:</h2>");
+document.write(daysOfWeek.slice(5).reverse().join(", "));
