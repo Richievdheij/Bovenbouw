57bbc6b9e2ffcebbf0ff580ccf34ade3d0a75090
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-03-20 12:25:02 +0100

    personenregister

--- Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-5/personenregister.js
+++ Fase-1 (FE+BE)/1-Front-end/Javascript/Lab-5/personenregister.js
@@ -1,39 +1,62 @@
-// Haal de gegevens op uit het JSON-bestand
-const persoon = {
-  "voornaam": "Piet",
-  "achternaam": "Heijn",
-  "nationaliteit": "Nederlandse",
-  "leeftijd": 47,
-  "gewicht": 86
-};
-
-// Haal de gegevens op uit het JSON-bestand
-const persoon2 = {
-  "voornaam": "Masud",
-  "achternaam": "Mohammed",
-  "nationaliteit": "Iraans",
-  "leeftijd": 37,
-  "gewicht": 79
-};
-const persoon = [
-{voornaam: 'Piet', 'Masud'},
-{achternaam: 'Heijn', 'Mohammed'},
-{nationaliteit: 'Nederlandse', 'Iraans'},
-{leeftijd: '47', '37'},
-{gewicht: '86', '79'}
-];
-
-
-// Zoek de HTML-elementen op basis van hun ID
-const voornaamElement = document.getElementById('voornaam');
-const achternaamElement = document.getElementById('achternaam');
-const nationaliteitElement = document.getElementById('nationaliteit');
-const leeftijdElement = document.getElementById('leeftijd');
-const gewichtElement = document.getElementById('gewicht');
-
-// Vul de HTML-elementen met de gegevens van de persoon
-voornaamElement.innerText = persoon.voornaam;
-achternaamElement.innerText = persoon.achternaam;
-nationaliteitElement.innerText = persoon.nationaliteit;
-leeftijdElement.innerText = persoon.leeftijd;
-gewichtElement.innerText = persoon.gewicht;
+personen = [
+{
+    "voornaam": "Piet",
+    "achternaam": "Heijn",
+    "nationaliteit": "Nederlandse",
+    "leeftijd": 47,
+    "gewicht": 86
+ },
+ {
+    "voornaam": "Masud",
+    "achternaam": "Mohammed",
+    "nationaliteit": "Iraans",
+    "leeftijd": 37,
+    "gewicht": 79
+ },
+ {
+    "voornaam": "Marie",
+    "achternaam": "Visser",
+    "nationaliteit": "Belgische",
+    "leeftijd": 42,
+    "gewicht": 69
+ },
+ {
+    "voornaam": "Klaas",
+    "achternaam": "Wever",
+    "nationaliteit": "Nederlandse",
+    "leeftijd": 73,
+    "gewicht": 85
+ },
+ {
+    "voornaam": "Bjorn",
+    "achternaam": "Hakke",
+    "nationaliteit": "Zweeds",
+    "leeftijd": 18,
+    "gewicht": 71
+ },
+ {
+    "voornaam": "Jouke",
+    "achternaam": "Dijkstra",
+    "nationaliteit": "Fries",
+    "leeftijd": 29,
+    "gewicht": 82
+ },
+ {
+    "voornaam": "Bo",
+    "achternaam": "Wáng",
+    "nationaliteit": "Chinees",
+    "leeftijd": 38,
+    "gewicht": 65
+ }]
+  personen.forEach(persoon => {
+    let personenElement = document.getElementById('personen');
+    personenElement.innerHTML += `
+    <div id="persoon">
+      <p> <b>Voornaam:</b> ${persoon.voornaam}</p>
+      <p> <b>Achternaam:</b> ${persoon.achternaam}</p>
+      <p> <b>Nationaliteit:</b> ${persoon.nationaliteit}</p>
+      <p> <b>Leeftijd:</b> ${persoon.leeftijd}</p>
+      <p> <b>Gewicht:</b> ${persoon.gewicht}</p>
+    </div>
+    `;
+  });
