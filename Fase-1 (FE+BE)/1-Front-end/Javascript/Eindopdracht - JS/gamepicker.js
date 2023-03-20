5507944665d105b666853f52fc19834e84321453
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-03-20 13:59:41 +0100

    Update gamepicker.js

--- Fase-1 (FE+BE)/1-Front-end/Javascript/Eindopdracht - JS/gamepicker.js
+++ Fase-1 (FE+BE)/1-Front-end/Javascript/Eindopdracht - JS/gamepicker.js
@@ -1,78 +1,193 @@
-personen = [
-{
-    "voornaam": "Piet",
-    "achternaam": "Heijn",
-    "nationaliteit": "Nederlandse",
-    "leeftijd": 47,
-    "gewicht": 86
- },
- {
-    "voornaam": "Masud",
-    "achternaam": "Mohammed",
-    "nationaliteit": "Iraans",
-    "leeftijd": 37,
-    "gewicht": 79
- },
- {
-    "voornaam": "Marie",
-    "achternaam": "Visser",
-    "nationaliteit": "Belgische",
-    "leeftijd": 42,
-    "gewicht": 69
- },
- {
-    "voornaam": "Klaas",
-    "achternaam": "Wever",
-    "nationaliteit": "Nederlandse",
-    "leeftijd": 73,
-    "gewicht": 85
- },
- {
-    "voornaam": "Bjorn",
-    "achternaam": "Hakke",
-    "nationaliteit": "Zweeds",
-    "leeftijd": 18,
-    "gewicht": 71
- },
- {
-    "voornaam": "Jouke",
-    "achternaam": "Dijkstra",
-    "nationaliteit": "Fries",
-    "leeftijd": 29,
-    "gewicht": 82
- },
- {
-    "voornaam": "Bo",
-    "achternaam": "Wáng",
-    "nationaliteit": "Chinees",
-    "leeftijd": 38,
-    "gewicht": 65
- }]
-// Maak een functie die haalt op de nummers van mijn inputvelden en zet deze in een array.
-function getNumbers() {
-   let numbers = [];
-   let number1 = document.getElementById('number1').value;
-   let number2 = document.getElementById('number2').value;
-   numbers.push(number1, number2);
-}
-
-function getPeople(){
-   let requiredAge = document.getElementById('requiredAge').value; // input ding
-   personen.forEach(persoon => {
-      if (persoon.leeftijd > requiredAge)
-         {
-         let personenElement = document.getElementById('personen');
-         personenElement.innerHTML += `
-         <div id="persoon">
-            <p> <b>Voornaam:</b> ${persoon.voornaam}</p>
-            <p> <b>Achternaam:</b> ${persoon.achternaam}</p>
-            <p> <b>Nationaliteit:</b> ${persoon.nationaliteit}</p>
-            <p> <b>Leeftijd:</b> ${persoon.leeftijd}</p>
-            <p> <b>Gewicht:</b> ${persoon.gewicht}</p>
-         </div>
-         `;
-         };
-      });
+var games = [
+   {
+       "title": "Counter-Strike: Global Offensive",
+       "price": 0.00,
+       "genre": "FPS",
+       "rating": 4
+   },
+   {
+       "title": "Dota 2",
+       "price": 0.00,
+       "genre": "MOBA",
+       "rating": 3
+   },
+   {
+       "title": "Goose Goose Duck",
+       "price": 4,99,
+       "genre": "Action",
+       "rating": 2
+   },
+   {
+       "title": "Apex Legends",
+       "price": 0.00,
+       "genre": "FPS",
+       "rating": 4
+   },
+   {
+       "title": "PUBG: BATTLEGROUNDS",
+       "price": 29.99,
+       "genre": "FPS",
+       "rating": 5
+   },
+   {
+       "title": "Lost Ark",
+       "price": 49.99,
+       "genre": "Action",
+       "rating": 1
+   },
+   {
+       "title": "Grand Theft Auto V",
+       "price": 29.99,
+       "genre": "FPS",
+       "rating": 3
+   },
+   {
+       "title": "Call of Duty®: Modern Warfare® II | Warzone™ 2.0",
+       "price": 19.99,
+       "genre": "FPS",
+       "rating": 3
+   },
+   {
+       "title": "Team Fortress 2",
+       "price": 0.00,
+       "genre": "FPS",
+       "rating": 5
+   },
+   {
+       "title": "Rust",
+       "price": 39.99,
+       "genre": "Action",
+       "rating": 5
+   },
+   {
+       "title": "Unturned",
+       "price": 0.00,
+       "genre": "RPG",
+       "rating": 1
+   },
+   {
+       "title": "ELDEN RING",
+       "price": 59.99,
+       "genre": "RPG",
+       "rating": 5
+   },
+   {
+       "title": "ARK: Survival Evolved",
+       "price": 10.00,
+       "genre": "RPG",
+       "rating": 1
+   },
+   {
+       "title": "War Thunder",
+       "price": 0.00,
+       "genre": "Simulation",
+       "rating": 2
+   },
+   {
+       "title": "Sid Meier's Civilization VI",
+       "price": 29.99,
+       "genre": "Simulation",
+       "rating": 3
+   {
+       "title": "Football Manager 2023",
+       "price": 59.99,
+       "genre": "Simulation",
+       "rating": 3
+   },
+   {
+       "title": "Warframe",
+       "price": 0.00,
+       "genre": "Looter-shooter",
+       "rating": 3
+   },
+   {
+       "title": "EA SPORTS™ FIFA 23",
+       "price": 59.99,
+       "genre": "Sport",
+       "rating": 1
+   },
+   {
+       "title": "Destiny 2",
+       "price": 0.00,
+       "genre": "FPS",
+       "rating": 5
+   },
+   {
+       "title": "Red Dead Redemption 2",
+       "price": 59.99,
+       "genre": "RPG",
+       "rating": 4
+   },
+   {
+       "title": "Tom Clancy's Rainbow Six Siege",
+       "price": 19.99,
+       "genre": "Simulation",
+       "rating": 3
+   },
+   {
+       "title": "The Witcher 3: Wild Hunt",
+       "price": 39.99,
+       "genre": "RPG",
+       "rating": 4
+   },
+   {
+       "title": "Terraria",
+       "price": 9.99,
+       "genre": "Sandbox",
+       "rating": 2
+   },
+   {
+       "title": "Stardew Valley",
+       "price": 14.99,
+       "genre": "Sandbox",
+       "rating": 1
+   },
+   {
+       "title": "Left 4 Dead 2",
+       "price": 9.99,
+       "genre": "FPS",
+       "rating": 4
+   },
+   {
+       "title": "Don't Starve Together",
+       "price": 5.09,
+       "genre": "RPG",
+       "rating": 3
+   },
+   {
+       "title": "MIR4",
+       "price": 19.99,
+       "genre": "RPG",
+       "rating": 3
+   },
+   {
+       "title": "PAYDAY 2",
+       "price": 9.99,
+       "genre": "Action",
+       "rating": 2
+   },
+   {
+       "title": "Path of Exile",
+       "price": 0.00,
+       "genre": "RPG",
+       "rating": 4
+   },
+   {
+       "title": "Project Zomboid",
+       "price": 14.99,
+       "genre": "Simulation",
+       "rating": 4
+   },
+   {
+       "title": "Valheim",
+       "price": 19.99,
+       "genre": "Sandbox",
+       "rating": 5
+   },
+   {
+       "title": "DayZ",
+       "price": 44.99,
+       "genre": "Simulation",
+       "rating": 3
    }
-
-
+];
