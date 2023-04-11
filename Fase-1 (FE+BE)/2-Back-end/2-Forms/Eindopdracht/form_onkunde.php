0f9aab0b2371ab77ce743eac9fe33967884efd11
Richievdheij <99058860@mydavinci.nl>
2023-04-11 15:20:19 +0200

    update

--- Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_onkunde.php
+++ Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_onkunde.php
@@ -1,38 +1,20 @@
 <?php
 
-$error = "";
-$result = "";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $vraag1 = $_POST["vraag1"];
+  $vraag2 = $_POST["vraag2"];
+  $vraag3 = $_POST["vraag3"];
+  $vraag4 = $_POST["vraag4"];
+  $vraag5 = $_POST["vraag5"];
+  $vraag6 = $_POST["vraag6"];
+  $vraag7 = $_POST["vraag7"];
 
-  // Valideer invoer en sla gegevens op
-  $vraag1 = clean_input($_POST["vraag1"]);
-  $vraag2 = clean_input($_POST["vraag2"]);
-  $vraag3 = clean_input($_POST["vraag3"]);
-  $vraag4 = clean_input($_POST["vraag4"]);
-  $vraag5 = clean_input($_POST["vraag5"]);
-  $vraag6 = clean_input($_POST["vraag6"]);
-  $vraag7 = clean_input($_POST["vraag7"]);
-
-  if (empty($vraag1) || empty($vraag2) || empty($vraag3) || empty($vraag4) || empty($vraag5) || empty($vraag6) || empty($vraag7)) {
-    $error = "Vul alle velden in";
-  } else {
-
-    // Maak grappige tekst met de gegevens
-    $result = "Er was eens iemand die graag " . $vraag1 . " wilde kunnen. 
-    Hij/zij kon echter alleen goed opschieten met " . $vraag2 . ". 
-    Het favoriete getal van deze persoon was " . $vraag3 . " en tijdens vakanties nam hij/zij altijd " . $vraag4 . " mee. 
-    Zijn/haar beste persoonlijke eigenschap was " . $vraag5 . ", maar helaas was zijn/haar slechtste eigenschap " . $vraag6 . ". 
-    Het ergste wat deze persoon kon overkomen was " . $vraag7 . ".";
-  }
-}
-
-// Functie om invoer schoon te maken
-function clean_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+  // plaats de uitgelezen formuliergegevens in een grappige tekst
+  echo "Er was eens iemand die graag $vraag1 wilde kunnen."     ;
+  echo "Hij/zij kon echter alleen goed opschieten met $vraag2. ";
+  echo "Het favoriete getal van deze persoon was $vraag3 en tijdens vakanties nam hij/zij altijd $vraag4 mee.";
+  echo "Zijn/haar beste persoonlijke eigenschap was $vraag5, maar helaas was zijn/haar slechtste eigenschap $vraag6.";
+  echo "Het ergste wat deze persoon kon overkomen was $vraag7";
 }
 
 ?>
--- Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_paniek.php
+++ Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_paniek.php
@@ -1,34 +1,24 @@
 <?php
 
-$error = "";
-$result = "";
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $vraag1 = $_POST["vraag1"];
+  $vraag2 = $_POST["vraag2"];
+  $vraag3 = $_POST["vraag3"];
+  $vraag4 = $_POST["vraag4"];
+  $vraag5 = $_POST["vraag5"];
+  $vraag6 = $_POST["vraag6"];
+  $vraag7 = $_POST["vraag7"];
+  $vraag7 = $_POST["vraag8"];
 
-  // Valideer invoer en sla gegevens op
-  $vraag1 = clean_input($_POST["vraag1"]);
-  $vraag2 = clean_input($_POST["vraag2"]);
-  $vraag3 = clean_input($_POST["vraag3"]);
-  $vraag4 = clean_input($_POST["vraag4"]);
-  $vraag5 = clean_input($_POST["vraag5"]);
-  $vraag6 = clean_input($_POST["vraag6"]);
-  $vraag7 = clean_input($_POST["vraag7"]);
-
-  if (empty($vraag1) || empty($vraag2) || empty($vraag3) || empty($vraag4) || empty($vraag5) || empty($vraag6) || empty($vraag7) || empty($vraag8)) {
-    $error = "Vul alle velden in";
-  } else {
-
-    // Maak grappige tekst met de gegevens van vraag 1 t/m 8
-    $result = 
-  }
-}
-
-// Functie om invoer schoon te maken
-function clean_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+  // plaats de uitgelezen formuliergegevens in een grappige tekst
+  echo "Laten we eens kijken naar jouw antwoorden: <br>";
+  echo "Je zou graag $vraag1 willen kunnen, maar helaas, dat gaat je nooit lukken.<br>";
+  echo "Met $vraag2 kun je goed opschieten? Dat is onwaarschijnlijk.<br>";
+  echo "$vraag3 is je favoriete getal? Serieus? Dat is het ergste antwoord dat we ooit hebben gehoord.<br>";
+  echo "Wat je altijd bij je hebt als je op vakantie gaat, is $vraag4? Dat klinkt nutteloos.<br>";
+  echo "Je beste persoonlijke eigenschap is $vraag5? Dat is een grapje, toch?<br>";
+  echo "Je slechtste persoonlijke eigenschap is $vraag6? Dat hadden we al wel gedacht.<br>";
+  echo "Het ergste dat je kan overkomen is $vraag7? Maak je geen zorgen, dat gaat zeker gebeuren.<br>";
 }
 
 ?>
