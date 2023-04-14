93fc0fa4a7d83989f876f1396e2d88c110f9f5a9
Richievdheij <99058860@mydavinci.nl>
2023-04-14 21:41:06 +0200

    update

--- Fase-1 (FE+BE)/2-Back-end/3-Templating/Eindopdracht/J2F1BELP3L2_includes/index.php
+++ Fase-1 (FE+BE)/2-Back-end/3-Templating/Eindopdracht/J2F1BELP3L2_includes/index.php
@@ -12,12 +12,47 @@
   <?php include("includes/header.php"); ?>
 
 	<!-- laad hier via php de juiste contentpagina in (vanuit de pages map) in. Welke geselecteerd moet worden kun je uit de URL halen (URL_Params).-->
-  <?php include('onderwerp1.php'); ?>
-  <?php include('onderwerp2.php'); ?>
-  <?php include('onderwerp3.php'); ?>
+  
+  <?php
+if(isset($_GET['content'])) {
+    switch($_GET['content']) {
+        case "index":
+            $content = "content/index.php";
+            break;
+        case "onderwerp1":
+            $content = "content/onderwerp1.php";
+            break;
+        case "onderwerp2":
+            $content = "content/onderwerp2.php";
+            break;
+        case "onderwerp3":
+            $content = "content/onderwerp3.php";
+            break;
+        default:
+            $content = "content/index.php";
+            break;
+    }
+} else {
+    $content = "content/index.php";
+}
+?>
+
+
+  <?php require('onderwerp1.php'); ?>
+  <?php echo . $title; ?>
+  <?php echo . $description; ?>
+  <?php echo . $image; ?>
+  <?php require('onderwerp2.php'); ?>
+  <?php echo . $title; ?>
+  <?php echo . $description; ?>
+  <?php echo . $image; ?>
+  <?php require('onderwerp3.php'); ?>
+  <?php echo . $title; ?>
+  <?php echo . $description; ?>
+  <?php echo . $image; ?>
 	
 	<!-- laad hier via php je footer in (vanuit je includes map)-->
-  <?php include('footer.php'); ?>
+  <?php include('includes/footer.php'); ?>
 
 </body>
 </html>
--- Fase-1 (FE+BE)/2-Back-end/3-Templating/Eindopdracht/J2F1BELP3L2_includes/pages/onderwerp3.php
+++ Fase-1 (FE+BE)/2-Back-end/3-Templating/Eindopdracht/J2F1BELP3L2_includes/pages/onderwerp3.php
@@ -1,3 +1,21 @@
-<?php
+<?php 
+$title = "Titel van het onderwerp";
+$description = "Beschrijving van het onderwerp met ten minste 200 woorden.";
+$image = "pad/naar/afbeelding.jpg";
 ?>
+
 <!-- jouw HTML met de inhoud over onderwerp 3 komt hier... -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onderwerp 3</title>
+</head>
+<body>
+    <h1>Deze pagina gaat over onderwerp 3</h1>
+    <p>Lorem ipsum dolor sit amet co nsectetur adipisicing elit. Quisquam, quod.</p>
+    <img src="" alt="random foto">
+</body>
+</html>
