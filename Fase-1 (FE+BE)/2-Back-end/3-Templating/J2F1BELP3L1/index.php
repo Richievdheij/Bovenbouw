a76ff30081c7dae3e385425f50654881f00f6dc5
Richievdheij <99058860@mydavinci.nl>
2023-04-14 18:02:57 +0200

    J2F1BELP3L1

--- Fase-1 (FE+BE)/2-Back-end/3-Templating/J2F1BELP3L1/index.php
+++ Fase-1 (FE+BE)/2-Back-end/3-Templating/J2F1BELP3L1/index.php
@@ -2,5 +2,7 @@
 
 require('variables.php');
 echo "Mijn naam is " . $naam;
+echo "<br>";
+echo "Mijn favoriete fruit zijn: " . implode(", ", $fruit);
 
 ?>
--- Fase-1 (FE+BE)/2-Back-end/3-Templating/J2F1BELP3L1/variables.php
+++ Fase-1 (FE+BE)/2-Back-end/3-Templating/J2F1BELP3L1/variables.php
@@ -1,5 +1,6 @@
 <?php
 
 $naam = "Richie van der Heij";
+$fruit = array("appel", "banaan", "aardbei");
 
 ?>
