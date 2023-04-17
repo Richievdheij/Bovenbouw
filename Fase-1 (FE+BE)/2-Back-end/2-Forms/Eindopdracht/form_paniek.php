27bfce410b9295c6fb3f615ca5247ab266b8f5fb
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-04-17 12:09:36 +0200

    update

--- Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_paniek.php
+++ Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_paniek.php
@@ -14,14 +14,46 @@
     include 'includes/header.php';
 
     // Body
-    $vraag1t = $_POST["vraag1t"];
-    $vraag2t = $_POST["vraag2t"];
-    $vraag3t = $_POST["vraag3t"];
-    $vraag4t = $_POST["vraag4t"];
-    $vraag5t = $_POST["vraag5t"];
-    $vraag6t = $_POST["vraag6t"];
-    $vraag7t = $_POST["vraag7t"];
-    $vraag8t = $_POST["vraag8t"];
+    $vraag1t = isset($_POST["vraag1t"]) ? $_POST["vraag1t"] : "";
+    $vraag2t = isset($_POST["vraag2t"]) ? $_POST["vraag2t"] : "";
+    $vraag3t = isset($_POST["vraag3t"]) ? $_POST["vraag3t"] : "";
+    $vraag4t = isset($_POST["vraag4t"]) ? $_POST["vraag4t"] : "";
+    $vraag5t = isset($_POST["vraag5t"]) ? $_POST["vraag5t"] : "";
+    $vraag6t = isset($_POST["vraag6t"]) ? $_POST["vraag6t"] : "";
+    $vraag7t = isset($_POST["vraag7t"]) ? $_POST["vraag7t"] : "";
+    $vraag7t = isset($_POST["vraag8t"]) ? $_POST["vraag8t"] : "";
+
+    if(empty($vraag1t) || !is_string($vraag1t)){
+      // Error handling for question 1
+    }
+
+    if(empty($vraag2t) || !is_string($vraag2t)){
+      // Error handling for question 2
+    }
+
+    if(empty($vraag3t) || !is_numeric($vraag3t)){
+      // Error handling for question 3
+    }
+
+    if(empty($vraag4t) || !is_string($vraag4t)){
+      // Error handling for question 4
+    }
+
+    if(empty($vraag5t) || !is_string($vraag5t)){
+      // Error handling for question 5
+    }
+
+    if(empty($vraag6t) || !is_string($vraag6t)){
+      // Error handling for question 6
+    }
+
+    if(empty($vraag7t) || !is_string($vraag7t)){
+      // Error handling for question 7
+    }
+
+    if(empty($vraag8t) || !is_string($vraag8t)){
+      // Error handling for question 7
+    }
     ?>
 
     <p>
@@ -37,10 +69,11 @@
     ?>
     </p>
 
-  <?php
-    // Footer
-    include 'includes/footer.php';
-  ?>
+    <?php
+      // Footer
+      include 'includes/footer.php';
+    ?>
+
   </div>
 </body>
 </html>
