5487553ca0ea1574a8c86749973f9564908f9cdc
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-04-17 13:36:24 +0200

    update

--- Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_onkunde.php
+++ Fase-1 (FE+BE)/2-Back-end/2-Forms/Eindopdracht/form_onkunde.php
@@ -13,43 +13,51 @@
     // Header
     include 'includes/header.php'; 
 
-    // Body
-    $vraag1t = isset($_POST["vraag1t"]) ? $_POST["vraag1t"] : "";
-    $vraag2t = isset($_POST["vraag2t"]) ? $_POST["vraag2t"] : "";
-    $vraag3t = isset($_POST["vraag3t"]) ? $_POST["vraag3t"] : "";
-    $vraag4t = isset($_POST["vraag4t"]) ? $_POST["vraag4t"] : "";
-    $vraag5t = isset($_POST["vraag5t"]) ? $_POST["vraag5t"] : "";
-    $vraag6t = isset($_POST["vraag6t"]) ? $_POST["vraag6t"] : "";
-    $vraag7t = isset($_POST["vraag7t"]) ? $_POST["vraag7t"] : "";
+    // Validate form submission
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      $vraag1t = isset($_POST["vraag1t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag1t"]))) : "";
+      $vraag2t = isset($_POST["vraag2t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag2t"]))) : "";
+      $vraag3t = isset($_POST["vraag3t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag3t"]))) : "";
+      $vraag4t = isset($_POST["vraag4t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag4t"]))) : "";
+      $vraag5t = isset($_POST["vraag5t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag5t"]))) : "";
+      $vraag6t = isset($_POST["vraag6t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag6t"]))) : "";
+      $vraag7t = isset($_POST["vraag7t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag7t"]))) : "";
+      $vraag8t = isset($_POST["vraag8t"]) ? trim(htmlspecialchars(stripslashes($_POST["vraag8t"]))) : "";
 
-    if(empty($vraag1t) || !is_string($vraag1t)){
-      // Error handling for question 1
+      $errors = array();
+
+      if(empty($vraag1t) || !is_string($vraag1t)){
+        $errors[] = "Vraag 1 is onjuist ingevuld.";
+      }
+
+      if(empty($vraag2t) || !is_string($vraag2t)){
+        $errors[] = "Vraag 2 is onjuist ingevuld.";
+      }
+
+      if(empty($vraag3t) || !is_numeric($vraag3t)){
+        $errors[] = "Vraag 3 is onjuist ingevuld.";
+      }
+
+      if(empty($vraag4t) || !is_string($vraag4t)){
+        $errors[] = "Vraag 4 is onjuist ingevuld.";
+      }
+
+      if(empty($vraag5t) || !is_string($vraag5t)){
+        $errors[] = "Vraag 5 is onjuist ingevuld.";
+      }
+
+      if(empty($vraag6t) || !is_string($vraag6t)){
+        $errors[] = "Vraag 6 is onjuist ingevuld.";
+      }
+
+      if(empty($vraag7t) || !is_string($vraag7t)){
+        $errors[] = "Vraag 7 is onjuist ingevuld.";
+      }
+
+      if(empty($vraag8t) || !is_string($vraag8t)){
+        $errors[] = "Vraag 8 is onjuist ingevuld.";
+      }
     }
-
-    if(empty($vraag2t) || !is_string($vraag2t)){
-      // Error handling for question 2
-    }
-
-    if(empty($vraag3t) || !is_numeric($vraag3t)){
-      // Error handling for question 3
-    }
-
-    if(empty($vraag4t) || !is_string($vraag4t)){
-      // Error handling for question 4
-    }
-
-    if(empty($vraag5t) || !is_string($vraag5t)){
-      // Error handling for question 5
-    }
-
-    if(empty($vraag6t) || !is_string($vraag6t)){
-      // Error handling for question 6
-    }
-
-    if(empty($vraag7t) || !is_string($vraag7t)){
-      // Error handling for question 7
-    }
-
     ?>
 
     <!-- plaats de uitgelezen formuliergegevens in een grappige tekst -->
