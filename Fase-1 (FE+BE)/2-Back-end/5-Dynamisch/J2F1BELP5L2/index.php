9734f0fa6f94ea002157b5bcea0552ac021a741e
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-05-15 14:37:08 +0200

    Update index.php

--- Fase-1 (FE+BE)/2-Back-end/5-Dynamisch/J2F1BELP5L2/index.php
+++ Fase-1 (FE+BE)/2-Back-end/5-Dynamisch/J2F1BELP5L2/index.php
@@ -1,17 +1,46 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "rootmysql";
+$password = "mysql";
+$dbname = "myDBPDO";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+
+  // prepare sql and bind parameters
+  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email)
+  VALUES (:firstname, :lastname, :email)");
+  $stmt->bindParam(':firstname', $firstname);
+  $stmt->bindParam(':lastname', $lastname);
+  $stmt->bindParam(':email', $email);
+
+  // insert a row
+  $firstname = "John";
+  $lastname = "Doe";
+  $email = "john@example.com";
+  $stmt->execute();
+
+  // insert another row
+  $firstname = "Mary";
+  $lastname = "Moe";
+  $email = "mary@example.com";
+  $stmt->execute();
+
+  // insert another row
+  $firstname = "Julie";
+  $lastname = "Dooley";
+  $email = "julie@example.com";
+  $stmt->execute();
+
+  echo "New records created successfully";
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+  echo "Error: " . $e->getMessage();
 }
+$conn = null;
 ?>
+
 
 <?php 
 $title = "Roblox";
