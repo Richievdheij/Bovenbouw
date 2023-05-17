56b709a54321631fb4085bfbc0e02ddc7af0d0dc
Richievdheij <91939456+Richievdheij@users.noreply.github.com>
2023-05-17 16:49:37 +0200

    update

--- Fase-1 (FE+BE)/2-Back-end/5-Dynamisch/J2F1BELP5O1/index.php
+++ Fase-1 (FE+BE)/2-Back-end/5-Dynamisch/J2F1BELP5O1/index.php
@@ -23,13 +23,43 @@ try {
 
 <!-- HTML -->
 
-<?php foreach ($characters as $character): ?>
-    <div class="character">
-        <img src="resources/images/<?php echo $character['avatar']; ?>" alt="<?php echo $character['name']; ?> Avatar">
-        <h2><?php echo $character['name']; ?></h2>
-        <p><strong>Levenskracht:</strong> <?php echo $character['health']; ?></p>
-        <p><strong>Aanvalskracht:</strong> <?php echo $character['attack']; ?></p>
-        <p><strong>Verdediging:</strong> <?php echo $character['defense']; ?></p>
-        <a href="character.php?id=<?php echo $character['id']; ?>">Details</a>
-    </div>
-<?php endforeach; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All Characters</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="vormgeving/resources/css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<header>
+    <!-- Het totaal aantal characters moet zichtbaar zijn op de plaats van [X].  -->
+    <h1>Alle <?php echo count($characters); ?> characters uit de database</h1>
+</header>
+<div id="container">
+    <?php
+    // Voor elk character in de characters array, toon een character
+    foreach($characters as $character) {
+        echo "<a class='item' href='character.php?id=" . $character['id'] . "'>";
+        echo "<div class='left'>";
+        echo "<img class='avatar' src='vormgeving/resources/images/" . $character['avatar'] . "' />";
+        echo "</div>";
+        echo "<div class='right'>";
+        echo "<h2>" . $character['name'] . "</h2>";
+        echo "<div class='stats'>";
+        echo "<ul class='fa-ul'>";
+        echo "<li><span class='fa-li'><i class='fas fa-heart'></i></span>" . $character['health'] . "</li>";
+        echo "<li><span class='fa-li'><i class='fas fa-fist-raised'></i></span>" . $character['attack'] . "</li>";
+        echo "<li><span class='fa-li'><i class='fas fa-shield-alt'></i></span>" . $character['defense'] . "</li>";
+        echo "</ul>";
+        echo "</div>";
+        echo "</div>";
+        echo "<div class='detailButton'><i class='fas fa-search'></i> bekijk</div>";
+        echo "</a>";
+    }
+    ?>
+</div>
+<!-- Geef footer gegevens via PHP -->
+<footer> <?php echo "&copy; [Richie van der Heij] 2023" ?> </footer>
+</body>
+</html>
